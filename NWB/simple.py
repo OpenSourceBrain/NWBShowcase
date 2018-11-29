e3f1e23ab2c2f53ab19e85d84aa7f0dbb3015eaa
Padraig Gleeson <p.gleeson@gmail.com>
2018-11-29 12:05:35 +0000

    Works with latest pynwb

--- NWB/simple.py
+++ NWB/simple.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+
+from dateutil.tz import tzlocal
 from pynwb import NWBFile
 
-start_time = datetime(2018, 4, 3, 11, 0, 0)
-create_date = datetime(2018, 4, 15, 12, 0, 0)
+start_time = datetime(2018, 4, 3, 11, tzinfo=tzlocal())
+create_date = datetime(2018, 4, 15, 12, tzinfo=tzlocal())
 
-nwbfile = NWBFile('PyNWB tutorial', 'demonstrate NWBFile basics', 'NWB123', start_time,
+nwbfile = NWBFile('PyNWB tutorial', 'NWB123', start_time,
                   file_create_date=create_date)
 
 from pynwb import NWBHDF5IO
