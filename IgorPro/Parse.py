65287402124f008e422c53a91e097b9c63e2c87f
Padraig Gleeson <p.gleeson@gmail.com>
2019-09-02 15:47:50 +0100

    Example nwb

--- IgorPro/Parse.py
+++ IgorPro/Parse.py
@@ -1,7 +1,9 @@
-
+from datetime import datetime
+from dateutil.tz import tzlocal
 import os.path
 from igor.packed import load as loadpxp
 from pprint import pprint
+import numpy
 
 filename = '141117c2.pxp'
 filename = '141210c3.pxp'
@@ -12,6 +14,20 @@ records,filesystem = loadpxp(path)
 
 
 import matplotlib.pyplot as plt
+
+
+import pynwb
+
+dataset = filename
+
+start_time = datetime(2019, 1, 1, 11, tzinfo=tzlocal())
+
+nwbfile = pynwb.NWBFile('Golgi cell recordings', filename, datetime.now(tzlocal()),
+                  experimenter='Frederic Lanore',
+                  lab='Silver Lab',
+                  institution='UCL',
+                  experiment_description=('Electrophysiological recordings from cerebellar Golgi cells'),
+                  session_id='??')
 
 def data_info(wdata):
     print('Data %s, shape: %s, %s -> %s (max %s, min %s)'%(type(wdata), wdata.shape, wdata[0], wdata[-1], wdata.max(), wdata.min()))
@@ -74,10 +90,24 @@ for d in filesystem['root']:
                 #print '..........'
                 print notes
                 '''
-                data_info(wave['wData'])
+                data = wave['wData']
+                data_info(data)
                 print
+                
+                timestamps = numpy.array([s*0.1 for s in range(10000)])
+                data_info(timestamps)
+                
+                ts = pynwb.TimeSeries('%s_%s'%(folder,name), 
+                              data, 
+                              'mV', 
+                              timestamps=timestamps, 
+                              description='%s, membrane potential'%name,
+                              comments='Extracted from IgorPro file: %s'%filename)
 
-                voltages[folder].plot(wave['wData'])
+                nwbfile.add_acquisition(ts)
+
+
+                voltages[folder].plot(timestamps,data)
                 
             if 'Step_CC' in dd:
                 for ddd in filesystem['root'][d][dd]:
@@ -110,5 +140,12 @@ for d in filesystem['root']:
 for aa in [added, added_stim]:
     for a in aa:
         print('All added to %s (%i): %s'%(a,len(aa[a]),aa[a]))
+        
+        
+nwb_file_name = '%s.nwb'%filename.split('.')[0]
+io = pynwb.NWBHDF5IO(nwb_file_name, mode='w')
+io.write(nwbfile)
+io.close()
+print("Written NWB file to %s"%nwb_file_name)
     
 plt.show()
